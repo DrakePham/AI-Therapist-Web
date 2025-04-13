2644fa130fb3bb9afd47f207a1b97bf72408ec78
DrakePham <89468358+DrakePham@users.noreply.github.com>
2025-04-13 14:23:55 +0700

    ai therapist hello world

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import { Nav } from "@/components/Nav";
+import { cn } from "@/utils";
+
+export const metadata: Metadata = {
+  title: "HGTInf",
+  description: "AI Therapist by HGTInf",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          GeistSans.variable,
+          GeistMono.variable,
+          "flex flex-col min-h-screen"
+        )}
+      >
+        <Nav />
+        {children}
+      </body>
+    </html>
+  );
+}
